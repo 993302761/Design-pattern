cbc5ffe70e6098952f204cc2c730e7c53b00a731
993302761 <993302761@qq.com>
2023-01-05 01:58:31 +0800

    单例模式学习完成

---
+++ src/Singleton_pattern/Singleton4.java
@@ -0,0 +1,30 @@
+package Singleton_pattern;
+
+/**
+ * Singleton_pattern.Singleton（单例）
+ * 懒汉式单例（在首次使用时创建）
+ * 双重检查锁方式
+ */
+public class Singleton4 {
+
+
+    //1.私有构造方法
+    private Singleton4(){}
+
+    //2.在本类中创建该类对象
+    //在多线程的情况下有可能出现空指针问题，原因是JVM在实例化对象的时候会进行优化的执行重排序,需要volatile来保证可见性和有序性
+    //可以保证在多线程的情况下线程安全且不会有性能问题
+    private static volatile Singleton4 s;
+
+    //3.提供一个公共的访问方法，让外界可以获取该对象
+    public static Singleton4 newSingleton(){
+        if (s==null){
+            synchronized (Singleton4.class){
+                if (s==null){
+                    s=new Singleton4();
+                }
+            }
+        }
+        return s;
+    }
+}
