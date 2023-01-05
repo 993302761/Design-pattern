a672acf5d5d325e98096a17cf85564c3e5fb98a6
993302761 <993302761@qq.com>
2023-01-05 23:55:59 +0800

    工厂模式学习完成

---
+++ out/production/Design_pattern/src/Prototype_pattern/Prototype.java
@@ -0,0 +1,45 @@
+package Prototype_pattern;
+
+public class Prototype implements Cloneable{
+
+
+    /**
+     * 浅克隆
+     */
+//    private String data;
+//
+//    public String getData() {
+//        return data;
+//    }
+//
+//    public void setData(String data) {
+//        this.data = data;
+//    }
+
+    /**
+     * 深克隆
+     */
+    private Data data;
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public void set1(String d){
+        data.setData(d);
+    }
+
+    public Prototype() {
+        System.out.println("创建成功");
+    }
+
+    @Override
+    public Prototype clone() throws CloneNotSupportedException {
+        System.out.println("原型复制成功");
+        return (Prototype) super.clone();
+    }
+}
