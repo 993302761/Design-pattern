2f8033cc48d0b5c1a7751451f4976d9848a74d76
993302761 <993302761@qq.com>
2023-01-08 15:11:26 +0800

    外观模式学习完成

---
+++ src/main/java/Facade_pattern/TV.java
@@ -0,0 +1,12 @@
+package Facade_pattern;
+
+public class TV {
+
+    public void on(){
+        System.out.println("开电视");
+    }
+
+    public void off(){
+        System.out.println("关电视");
+    }
+}
