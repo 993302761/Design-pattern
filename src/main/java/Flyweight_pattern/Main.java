cb571a303916b59d23c508b3db7ae2df71a4afca
993302761 <993302761@qq.com>
2023-01-09 15:04:44 +0800

    享元模式学习完成

--- src/main/java/Flyweight_pattern/Main.java
+++ src/main/java/Flyweight_pattern/Main.java
@@ -19,5 +19,16 @@ public class Main {
         //两次获取到的l图形是同一个对象
         System.out.println(l);
         System.out.println(z);
+
+
+//        //  Integer会默认先创建并缓存-128~127之间数的Integer对象，当调用ValueOf时，如果参数在-128~127之间则计算下标并从缓存中返回，否则则创建一个新的Integer对象
+//        Integer i1=127;
+//        Integer i2=127;
+//        System.out.println(i1==i2);
+//
+//        Integer i3=128;
+//        Integer i4=128;
+//        System.out.println(i3==i4);
+
     }
 }
