b0d5ab772050703b4a0818b7c4b9765f76875e52
993302761 <993302761@qq.com>
2023-01-10 09:54:36 +0800

    命令模式学习完成

---
+++ src/main/java/Command_pattern/Command.java
@@ -0,0 +1,8 @@
+package Command_pattern;
+
+/**
+ * 抽象命令类
+ */
+public interface Command {
+    void execute();
+}
---
+++ src/main/java/Command_pattern/Cook.java
@@ -0,0 +1,11 @@
+package Command_pattern;
+
+/**
+ * 厨师类
+ */
+public class Cook {
+
+    public void makeFood(String name,int num){
+        System.out.println(name+" "+num+"份");
+    }
+}
