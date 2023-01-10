4fd8864c6adbdddd5a76223341146a1e8d9e80c2
993302761 <993302761@qq.com>
2023-01-10 13:56:34 +0800

    观察者模式学习完成

---
+++ src/main/java/Oberver_pattern/ObServer.java
@@ -0,0 +1,9 @@
+package Oberver_pattern;
+
+/**
+ * 抽象观察者
+ */
+public interface ObServer {
+
+    void update(String s);
+}
---
+++ src/main/java/Oberver_pattern/User.java
@@ -0,0 +1,26 @@
+package Oberver_pattern;
+
+/**
+ * 用户
+ */
+public class User  implements  ObServer{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String s) {
+        System.out.println(name+"  "+s);
+    }
+}
