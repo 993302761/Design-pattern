bcfea8534588b5a38c0860b12bceb3a1b269edcb
993302761 <993302761@qq.com>
2023-01-11 14:35:55 +0800

    解释器模式学习完成

---
+++ src/main/java/Interpreter_pattern/AbstractExpression.java
@@ -0,0 +1,9 @@
+package Interpreter_pattern;
+
+/**
+ * 抽象表达式类
+ */
+public abstract class AbstractExpression {
+
+    public abstract int interpret(Context context);
+}
---
+++ src/main/java/Interpreter_pattern/Plus.java
@@ -0,0 +1,28 @@
+package Interpreter_pattern;
+
+/**
+ *
+ * 加法表达式类
+ */
+public class Plus extends AbstractExpression{
+
+    private AbstractExpression left;
+
+    private AbstractExpression right;
+
+    public Plus(AbstractExpression left, AbstractExpression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + left + "+" + right + ')';
+
+    }
+
+    @Override
+    public int interpret(Context context) {
+        return left.interpret(context)+right.interpret(context);
+    }
+}
